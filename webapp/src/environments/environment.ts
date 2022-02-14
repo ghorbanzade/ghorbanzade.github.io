6dc9847714bb4bbef8c87ed53639d8fd5efcfe13
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-02-14 14:34:43 -0600

    upgrade to angular v12

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -10,4 +10,4 @@ export const environment = {
  * For easier debugging in development mode, ignore zone related error stack
  * frames such as `zone.run`, `zoneDelegate.invokeTask`.
  */
-import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
