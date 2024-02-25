1c0d9e8ce8c8a7ffeda7c17421f5715acaed7069
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2024-02-25 10:34:04 -0800

    add link to talks page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import Image from 'next/image';
 import Markdown from 'markdown-to-jsx';
 
 import profilePicture from '@/public/images/pejman.jpg';
-import SocialLinks from '@/src/components/social';
-import DarkModeButton from '@/src/components/darkMode';
+import DarkModeButton from '@/src/components/DarkModeButton';
+import NavBar from '@/src/components/NavBar';
+import SocialLinks from '@/src/components/SocialLinks';
 
 export default async function Page() {
   const aboutPage = await fetch(
@@ -14,30 +15,35 @@ export default async function Page() {
   const fileContent = await aboutPage.text()
   return (
     <main className="flex h-screen md:items-center">
-      <div className="mx-auto max-w-screen-lg md:grid md:grid-cols-4 md:gap-3">
-        <div className="rounded-lg border-slate-300 bg-white p-6 dark:border-slate-700 dark:bg-gradient-to-bl dark:from-black dark:to-slate-900 md:col-span-1 md:border">
-          <div className="space-y-4 md:flex md:h-full md:flex-wrap md:content-between">
-            <div className="hidden py-2 text-center md:block">
-              <Image
-                className="rounded-lg"
-                src={profilePicture}
-                alt="Pejman Ghorbanzade"
-              />
-            </div>
-            <div className="w-full text-center">
-              <h1 className="text-lg font-semibold text-slate-900 dark:text-slate-200">Pejman Ghorbanzade</h1>
-              <h2 className="text-slate-600 dark:text-slate-300">Software Engineer</h2>
-            </div>
-            <SocialLinks />
-          </div>
+      <div className="mx-auto max-w-screen-lg">
+        <div className='py-6 justify-center flex'>
+          <NavBar />
         </div>
-        <div className="relative rounded-lg border-slate-300 bg-white p-6 dark:border-slate-700 dark:bg-gradient-to-br dark:from-black dark:to-slate-900 md:col-span-3 md:border">
-          <div className="absolute right-8 top-8">
-            <DarkModeButton />
+        <div className="md:grid md:grid-cols-4 md:gap-3">
+          <div className="rounded-lg border-slate-300 bg-white p-6 dark:border-slate-700 dark:bg-gradient-to-bl dark:from-black dark:to-slate-900 md:col-span-1 md:border">
+            <div className="space-y-4 md:flex md:h-full md:flex-wrap md:content-between">
+              <div className="hidden py-2 text-center md:block">
+                <Image
+                  className="rounded-lg"
+                  src={profilePicture}
+                  alt="Pejman Ghorbanzade"
+                />
+              </div>
+              <div className="w-full text-center">
+                <h1 className="text-lg font-semibold text-slate-900 dark:text-slate-200">Pejman Ghorbanzade</h1>
+                <h2 className="text-slate-600 dark:text-slate-300">Software Engineer</h2>
+              </div>
+              <SocialLinks />
+            </div>
           </div>
-          <Markdown className="wsl-mark prose prose-base prose-slate min-w-full dark:prose-invert">
-            {fileContent}
-          </Markdown>
+          <div className="relative rounded-lg border-slate-300 bg-white p-6 dark:border-slate-700 dark:bg-gradient-to-br dark:from-black dark:to-slate-900 md:col-span-3 md:border">
+            <div className="absolute right-8 top-8">
+              <DarkModeButton />
+            </div>
+            <Markdown className="wsl-mark prose prose-base prose-slate min-w-full dark:prose-invert">
+              {fileContent}
+            </Markdown>
+          </div>
         </div>
       </div>
     </main>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+// Copyright 2024 Pejman Ghorbanzade
+
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <nav className="space-x-2">
+      <Link
+        className="rounded-lg p-2 text-slate-600 dark:text-slate-400 dark:bg-slate-800 font-medium bg-slate-200"
+        href="/"
+      >
+        Home
+      </Link>
+      <a
+        className="rounded-lg p-2 text-slate-600 dark:text-slate-400 dark:hover:bg-slate-800 font-medium hover:bg-slate-200"
+        href="https://pejman.dev/talks"
+      >
+        Talks
+      </a>
+    </nav>
+  );
+}
